d31e63eec2d4048a44b5ea5158f793f583b9e618
Eric Myllyoja <ericmyllyoja@gmail.com>
2021-09-21 00:44:00 -0400

    Move version numbers to a static class.

---
+++ source/Enigma.hx
@@ -0,0 +1,11 @@
+/**
+ * Static class which contains compiler flags and other useful info.
+ */
+class Enigma extends MusicBeatState
+{
+	// Set to -prerelease on `develop` branch.
+	public static var ENGINE_SUFFIX:String = "-prerelease";
+	public static var ENGINE_VERSION:String = "0.1.0" + ENGINE_SUFFIX;
+	public static var ENGINE_NAME:String = "Enigma Engine";
+	public static var GAME_VERSION:String = "0.2.7.1";
+}
